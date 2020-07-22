4243a7e40b9640e1866fc2cbc071c737f31076d5
lh-123 <1840561548@qq.com>
2020-07-22 16:47:18 +0800

    停车场

---
+++ park/park_picture/src/main/java/org/company/parkpictureservice/mapper/LocalNumberMapper.java
@@ -0,0 +1,16 @@
+package org.company.parkpictureservice.mapper;
+
+import org.company.parkpictureservice.entity.LocalNumber;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 停车场地点 Mapper 接口
+ * </p>
+ *
+ * @author lih
+ * @since 2020-07-22
+ */
+public interface LocalNumberMapper extends BaseMapper<LocalNumber> {
+
+}
